a1a72f9d5fce193b30bc58ddea4bc627c298c38d
Boateng24 <56285760+Boateng24@users.noreply.github.com>
2021-10-01 02:17:35 -0700

    first commit

---
+++ homework_commented.js
@@ -0,0 +1,39 @@
+// Mary Rose's JavaScript Homework
+
+// 1. Write an if/else statement
+/* Newly added comment
+// good */
+var APPLE_QUANTITY = 5;
+ /* Newly added comment
+ // Not bad
+ */
+if (APPLE_QUANTITY < 5){
+  console.log("You have fewer than 5 apples.");
+} else {
+  console.log("You have 5 or more apples. Hurrah!");
+}
+
+// 2. Write a "for" loop
+/* Newly addad comment
+// better */
+for(i = 0; i < APPLE_QUANTITY; i ++){
+  console.log("WEEEEEEE! I'm looping!")
+}
+
+// 3. Write a function
+/* Newly added comment
+// work in progress */
+var sayHello = function(){
+  console.log("Hello!")
+}
+
+// 4. Create an object
+/** Newly added comment
+// keep it up */
+var mary = {
+  name: "Mary Rose",
+  codeNinja: true,
+  country: "UK"
+
+}
+
